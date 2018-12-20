90697d163373b0a37d48051dfe1750d926e1f9c4
Greg Polumbo <gpolumbo@broadinstitute.org>
2018-12-20 16:21:10 -0500

    setting key/value pairs on AliciaEntities

--- kv_store.py
+++ kv_store.py
@@ -1,17 +1,18 @@
 from google.appengine.ext import ndb
 
 
-class KVEntity(ndb.Expando):
+class AliciaEntity(ndb.Expando):
     pass
 
 
 class KVStore:
     def __init__(self, namespace, user_id):
-        self.storage_key = ndb.Key(KVEntity, user_id, namespace=namespace)
-        self.entity = KVEntity(key=self.storage_key)
+        self.storage_key = ndb.Key(AliciaEntity, user_id, namespace=namespace)
+        self.entity = AliciaEntity(key=self.storage_key)
 
     def write(self, data):
-        self.entity.data = data
+        for key, value in data.iteritems():
+            setattr(self.entity, key, value)
         self.entity.put()
 
     def read(self):
