bb7d1bdac17aeb3418b8fb7166d40e451d2f22d2
Greg Polumbo <gpolumbo@broadinstitute.org>
2018-12-20 15:58:18 -0500

    datastore works

--- alicia.py
+++ alicia.py
@@ -1,15 +1,16 @@
 import logging
-# from kv_store import KVStore
+from kv_store import KVStore
 
 
 class Alicia:
     def __init__(self):
         pass
 
-    def add_pair(self, user_id, key_value_pairs, namespace="default"):
-        # kv_store = KVStore(namespace, user_id)
+    @classmethod
+    def add_pair(cls, user_id, key_value_pairs, namespace="default"):
+        kv_store = KVStore(namespace, user_id)
         data = Alicia.key_value_pairs_to_dict(key_value_pairs)
-        logging.info("Data is: %s" % data)
+        kv_store.write(data)
 
     @classmethod
     def key_value_pairs_to_dict(cls, key_value_pairs):
